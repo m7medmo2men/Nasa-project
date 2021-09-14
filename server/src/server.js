60bbfacd953ff6af76b5ceec1d79f8c876f0fd42
Mohamed Momen <m7medmo2men@gmail.com>
2021-09-14 17:16:59 +0200

    Setup Mongoose Pacakge
    * You couldn't connect to the database because i removed the password from the connection string

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,30 @@
 const http = require("http");
+const mongoose = require("mongoose");
 const app = require("./app");
 const { loadPlanetsData } = require("./models/planetsModel");
 
 const port = 8000;
+const MONGO_URL =
+    "mongodb+srv://mo2a:password@cluster0.n2peb.mongodb.net/nasa?retryWrites=true&w=majority";
+
 const server = http.createServer(app);
 
+mongoose.connection.on("open", () => {
+    console.log("Connected Sucessfully to Database");
+});
+mongoose.connection.on("error", (err) => {
+    console.log("Failed To Connect To Database with error : " + err);
+});
+
 async function startServer() {
+    await mongoose.connect(MONGO_URL, {
+        // all these are options in the mongoDb driver that mongoose uses to connect to database
+        useNewUrlParser: true,
+        useFindAndModify: false, // Disble the outdated way of updating mongo data
+        useCreateIndex: true,
+        useUnifiedTopology: true, // MOngo will use the updated way to talk to the clusters
+    });
+
     await loadPlanetsData();
     server.listen(port, () => {
         console.log("Start Listening to requests at port " + port);
