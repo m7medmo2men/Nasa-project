edc4d88df4f716a611829d603ef6e7563f5d3a9c
Mohamed Momen <m7medmo2men@gmail.com>
2021-09-18 12:54:41 +0200

    Integrate Jest With MongoDB

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -3,74 +3,87 @@
 const supertest = require("supertest");
 const request = require("supertest");
 const app = require("../../app");
+const { mongoConnect, mongoDisconnect } = require("../../services/mongo");
 
-describe("Test Get /launches", () => {
-    test("it should respond with 200 sucess", async () => {
-        // actual test goes here
-        const response = await request(app)
-            .get("/launches")
-            .expect("Content-Type", /json/)
-            .expect(200);
-        // expect(response.statusCode).toBe(200);
+async function connect() {
+    await mongoConnect();
+}
+describe("Launches API", () => {
+    beforeAll(async () => {
+        await mongoConnect();
     });
-});
-
-describe("Test Post /launch", () => {
-    const completeLaunchData = {
-        mission: "USS Enterprise",
-        rocket: "NCC 1701-D",
-        target: "Kepler-62 f",
-        launchDate: "January 4, 2028",
-    };
-
-    const launchDataWithoutDate = {
-        mission: "USS Enterprise",
-        rocket: "NCC 1701-D",
-        target: "Kepler-62 f",
-    };
-
-    const launchDataWithInvalidDate = {
-        mission: "USS Enterprise",
-        rocket: "NCC 1701-D",
-        target: "Kepler-62 f",
-        launchDate: "zoot",
-    };
-
-    test("it should respond with 201 sucess", async () => {
-        const response = await request(app)
-            .post("/launches")
-            .send(completeLaunchData)
-            .expect("Content-Type", /json/)
-            .expect(201);
-
-        const requestDate = new Date(completeLaunchData.launchDate).valueOf();
-        const responseDate = new Date(response.body.launchDate).valueOf();
-        expect(responseDate).toBe(requestDate);
-
-        expect(response.body).toMatchObject(launchDataWithoutDate);
+    // connect();
+    afterAll(async () => {
+        await mongoDisconnect();
     });
-
-    test("It should catch missing required properties", async () => {
-        const response = await supertest(app)
-            .post("/launches")
-            .send(launchDataWithoutDate)
-            .expect("Content-Type", /json/)
-            .expect(400);
-
-        expect(response.body).toStrictEqual({
-            error: "Missing Required launch property",
+    describe("Test Get /launches", () => {
+        test("it should respond with 200 sucess", async () => {
+            // actual test goes here
+            const response = await request(app)
+                .get("/launches")
+                .expect("Content-Type", /json/)
+                .expect(200);
+            // expect(response.statusCode).toBe(200);
         });
     });
 
-    test("It should catch missing invalid Dates", async () => {
-        const response = await supertest(app)
-            .post("/launches")
-            .send(launchDataWithInvalidDate)
-            .expect("Content-Type", /json/)
-            .expect(400);
+    describe("Test Post /launch", () => {
+        const completeLaunchData = {
+            mission: "USS Enterprise",
+            rocket: "NCC 1701-D",
+            target: "Kepler-62 f",
+            launchDate: "January 4, 2028",
+        };
 
-        expect(response.body).toStrictEqual({
-            error: "Invalid Launch Date property",
+        const launchDataWithoutDate = {
+            mission: "USS Enterprise",
+            rocket: "NCC 1701-D",
+            target: "Kepler-62 f",
+        };
+
+        const launchDataWithInvalidDate = {
+            mission: "USS Enterprise",
+            rocket: "NCC 1701-D",
+            target: "Kepler-62 f",
+            launchDate: "zoot",
+        };
+
+        test("it should respond with 201 sucess", async () => {
+            const response = await request(app)
+                .post("/launches")
+                .send(completeLaunchData)
+                .expect("Content-Type", /json/)
+                .expect(201);
+
+            const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+            const responseDate = new Date(response.body.launchDate).valueOf();
+            expect(responseDate).toBe(requestDate);
+
+            expect(response.body).toMatchObject(launchDataWithoutDate);
+        });
+
+        test("It should catch missing required properties", async () => {
+            const response = await supertest(app)
+                .post("/launches")
+                .send(launchDataWithoutDate)
+                .expect("Content-Type", /json/)
+                .expect(400);
+
+            expect(response.body).toStrictEqual({
+                error: "Missing Required launch property",
+            });
+        });
+
+        test("It should catch missing invalid Dates", async () => {
+            const response = await supertest(app)
+                .post("/launches")
+                .send(launchDataWithInvalidDate)
+                .expect("Content-Type", /json/)
+                .expect(400);
+
+            expect(response.body).toStrictEqual({
+                error: "Invalid Launch Date property",
+            });
         });
     });
 });
---
+++ server/src/services/mongo.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const app = require("../app");
+
+const MONGO_URL =
+    "mongodb+srv://mo2a:password@cluster0.n2peb.mongodb.net/nasa?retryWrites=true&w=majority";
+
+mongoose.connection.once("open", () => {
+    console.log("Connected Sucessfully to Database");
+});
+mongoose.connection.on("error", (err) => {
+    console.log("Failed To Connect To Database with error : " + err);
+});
+
+async function mongoConnect() {
+    await mongoose.connect(MONGO_URL, {
+        // all these are options in the mongoDb driver that mongoose uses to connect to database
+        useNewUrlParser: true,
+        useFindAndModify: false, // Disble the outdated way of updating mongo data
+        useCreateIndex: true,
+        useUnifiedTopology: true, // MOngo will use the updated way to talk to the clusters
+    });
+}
+
+async function mongoDisconnect() {
+    await mongoose.disconnect();
+}
+
+module.exports = {
+    mongoConnect,
+    mongoDisconnect,
+};
